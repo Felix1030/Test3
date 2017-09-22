55d3ae7fe1382d3c99d9bf9b41bef1037f0f75ba
huaxiangyang <675579354@qq.com>
2017-09-22 15:22:28 +0800

    added AAA BBB CCC DDD

---
+++ app/src/main/java/com/emto/test3/AAA.java
@@ -0,0 +1,8 @@
+package com.emto.test3;
+
+/**
+ * Created by Master on 2017/9/22.
+ */
+
+public class AAA {
+}
---
+++ app/src/main/java/com/emto/test3/DDD.java
@@ -0,0 +1,9 @@
+package com.emto.test3;
+
+/**
+ * Created by Master on 2017/9/22.
+ */
+
+public class DDD {
+    public static final String str = "this is DDD";
+}
